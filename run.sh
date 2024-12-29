65fef30e44fb83abce1ec1edd37450fce730412a
63OR63 <iddqd@idkfa.com>
2024-12-29 12:15:43 +0500

    wip

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python3 --version > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-    echo "Python is not installed. Please install it from:"
-    echo "https://www.python.org/downloads/"
-    exit 1
-fi
-pythonw zonepaq &
-exit
